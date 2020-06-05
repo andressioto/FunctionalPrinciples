0222dd577dbd9608e10ae41ed1e2d87a5b5be5f6
andres <andressioto@live.com>
2020-06-05 14:29:44 +0200

    First Commit

--- AuditManager/Program.cs
+++ AuditManager/Program.cs
@@ -6,7 +6,7 @@ namespace AuditManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! ");
         }
     }
 }
